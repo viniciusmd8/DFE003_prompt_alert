4e7336746ba56c67eca125e6ad1380339855fb2e
unknown <viniciusmd8@gmail.com>
2023-10-01 22:11:11 -0300

    Descrição das modificações 2

--- src/main.js
+++ src/main.js
@@ -4,9 +4,9 @@
 // //   Processamento: calcular o dobro.
 // //   Saída: informar o resultado.
 
-// const numero = prompt('Insira o número que será dobrado');
-// const dobro = numero * 2;
-// alert(dobro);
+const numero = prompt('Insira o número que será dobrado');
+const dobro = numero * 2;
+alert(dobro);
 
 // // - Elabore um programa que calcule o valor do jantar, sendo o valor total do jantar composto pela conta junto com a taxa de atendimento do garçom.
 
@@ -16,31 +16,31 @@
 // //   Processamento: calcular junto com o valor do custo do garçom.
 // //   Saída: informar o resultado.
 
-// const valorJantar = parseFloat(prompt('Insira o valor do jantar:'));
-// const opcaoPagamento = parseInt(
-//   prompt('Escolha a opção de pagamento:\n1. 10%\n2. 15%\n3. 20%')
-// );
+const valorJantar = parseFloat(prompt('Insira o valor do jantar:'));
+const opcaoPagamento = parseInt(
+  prompt('Escolha a opção de pagamento:\n1. 10%\n2. 15%\n3. 20%')
+);
 
-// let taxa;
-// if (opcaoPagamento === 1) {
-//   taxa = 0.1; // 10%
-// } else if (opcaoPagamento === 2) {
-//   taxa = 0.15; // 15%
-// } else if (opcaoPagamento === 3) {
-//   taxa = 0.2; // 20%
-// } else {
-//   alert('Opção inválida');
-//   throw new Error('Opção inválida');
-// }
+let taxa;
+if (opcaoPagamento === 1) {
+  taxa = 0.1; // 10%
+} else if (opcaoPagamento === 2) {
+  taxa = 0.15; // 15%
+} else if (opcaoPagamento === 3) {
+  taxa = 0.2; // 20%
+} else {
+  alert('Opção inválida');
+  throw new Error('Opção inválida');
+}
 
-// const valorComTaxa = valorJantar * (1 + taxa);
-// alert(
-//   `O valor total do jantar com ${
-//     taxa * 100
-//   }% de taxa de serviço é de R$ ${valorComTaxa}`
-// );
+const valorComTaxa = valorJantar * (1 + taxa);
+alert(
+  `O valor total do jantar com ${
+    taxa * 100
+  }% de taxa de serviço é de R$ ${valorComTaxa}`
+);
 
-// - Elabore um programa para uma pizzaria, o qual leia o valor total de uma conta e quantos clientes vão pagá-la. Calcule e informe o valor a ser pago por cliente.
+- Elabore um programa para uma pizzaria, o qual leia o valor total de uma conta e quantos clientes vão pagá-la. Calcule e informe o valor a ser pago por cliente.
 
 // ~~~javascript
 
